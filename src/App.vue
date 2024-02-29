0ad61e872aa8a5cfc9b5b13d4573c8aab3431680
Wayfarerdesert <125356747+Wayfarerdesert@users.noreply.github.com>
2024-02-29 18:34:05 +0100

    Count Up Animation function is ready

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,89 @@
 <template>
-  <h1>{{ title }}</h1>
-  <h2>{{ subtitle }}</h2>
-  <h2>{{ subtitleII }}</h2>
-  <div id="app">
-    <input
-      v-model="refills"
-      type="number"
-      placeholder="Ingrese el número de recargas"
-    />
-    <button @click="calculateImpact">Calcular Impacto</button>
+  <div class="main">
+    <h1>{{ title }}</h1>
+    <h2>{{ subtitle }}</h2>
+    <h2>{{ subtitleII }}</h2>
 
-    <div class="impact-box" v-if="impactCalculated">
-      <h2>{{ impactBoxTitle }}</h2>
-      <div>
-        <h3>{{ bottlesSaved }}</h3>
-        <h2>Botellas de Plástico</h2>
-      </div>
-      <div>
-        <h3>{{ plasticSaved.toFixed(2) }}</h3>
-        <h2>Kg de Plástico</h2>
-      </div>
-      <div>
-        <h3>{{ carbonSaved.toFixed(2) }}</h3>
-        <h2>Kg de Dióxido de Carbono (CO2)</h2>
+    <div id="app">
+      <input
+        v-model="refills"
+        type="number"
+        placeholder="Ingrese el número de recargas"
+      />
+      <button @click="calculateImpact">Calcular Impacto</button>
+
+      <div class="wrapper">
+        <div class="impact-box" v-if="impactCalculated">
+          <h2>{{ impactBoxTitle }}</h2>
+          <div class="container-wrapper">
+            <div class="container">
+              <font-awesome-icon
+                :icon="['fas', 'bottle-water']"
+                :class="['icon-class', 'custom-color', 'custom-size']"
+              />
+              <!-- <img class="container-img" src="./../public/bottle-water.svg" alt="bottle-water"> -->
+              <h3 class="bottles num">{{ bottlesSaved }}</h3>
+              <h2>Botellas de Plástico</h2>
+            </div>
+            <div class="container">
+              <!-- <img src="./../public/recycle.svg" alt="recycle"> -->
+              <font-awesome-icon
+                :icon="['fas', 'recycle']"
+                :class="['icon-class', 'custom-color', 'custom-size']"
+              />
+              <h3 class="plastic num">{{ plasticSaved.toFixed(2) }}</h3>
+              <h2>Kg de Plástico</h2>
+            </div>
+            <div class="container">
+              <!-- <img src="./../public/weight-hanging.svg" alt="weight-hanging"> -->
+              <font-awesome-icon
+                :icon="['fas', 'weight-hanging']"
+                :class="['icon-class', 'custom-color', 'custom-size']"
+              />
+              <h3 class="carbon num">{{ carbonSaved.toFixed(2) }}</h3>
+              <h2>Kg de Dióxido de Carbono (CO2)</h2>
+            </div>
+          </div>
+          <button @click="shareImpact" v-if="impactCalculated">
+            Comparte tus logros
+          </button>
+          <!-- TODO +++++++++++++++++++++++++++++++++++++++++ -->
+          <!-- <font-awesome-icon
+            :icon="['fas', 'share']"
+            :class="['icon-class', 'custom-color', 'custom-size']"
+          />
+          <font-awesome-icon :icon="['fab', 'square-facebook']" />
+          <font-awesome-icon :icon="['fab', 'x-twitter']" /> -->
+          <!-- TODO +++++++++++++++++++++++++++++++++++++++++ -->
+        </div>
       </div>
     </div>
-
-    <button @click="shareImpact" v-if="impactCalculated">
-      Comparte tus logros
-    </button>
   </div>
 </template>
 
 <script>
+// import firebase from 'firebase/app'
+// import 'firebase/firestore'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import {
+  faBottleWater,
+  faRecycle,
+  faWeightHanging,
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(faBottleWater, faRecycle, faWeightHanging);
+
 export default {
   name: "App",
+  components: {
+    FontAwesomeIcon,
+  },
   data() {
     return {
-      title: "Refill",
-      subtitle: "¿Calcula que el impacto ambiental que generas al reutilizar tu botella de agua!",
+      title: "Refill Calculator",
+      subtitle:
+        "¡Calcula que el impacto ambiental que generas al reutilizar tu botella de agua!",
       subtitleII: "Introduce la cantidad de botellas que has rellenado:",
       impactBoxTitle: "Con esta acción has contribuido ahorrando:",
       refills: false,
@@ -60,6 +105,41 @@ export default {
 
       this.impactCalculated = true;
       this.animateCount();
+    },
+    animateCount() {
+      this.animateContainerCount(".num.bottles", this.bottlesSaved);
+      this.animateContainerCount(".num.plastic", this.plasticSaved);
+      this.animateContainerCount(".num.carbon", this.carbonSaved);
+    },
+    animateContainerCount(selector, endValue) {
+      let startValue = 0;
+      let maxInterval = 50; // Maximum interval
+      let minInterval = 10; // Minimum interval
+      // Calculate the duration based on the endValue number
+      let duration;
+      if (endValue <= 1) {
+        duration = Math.floor(minInterval / endValue);
+      } else {
+        duration = Math.floor(maxInterval / 1);
+      }
+
+      // Ensure duration is within reasonable bounds
+      duration = Math.max(minInterval, Math.min(maxInterval, duration));
+
+      let counter = setInterval(() => {
+        if (Number.isInteger(endValue)) {
+          startValue += 1; // Increment by 1 for integer values
+        } else {
+          startValue += 0.01; // Increment by a small value for float values
+        }
+        this.$el.querySelector(selector).textContent = startValue.toFixed(2); // Update the text content
+        if (startValue >= endValue) {
+          clearInterval(counter);
+        }
+      }, duration);
+    },
+    shareImpact() {
+      // Implement Firebase to generate and manage unique shareable links
     },
   },
 };
